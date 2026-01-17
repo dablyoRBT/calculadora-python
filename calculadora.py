a09841decff43be2b8e5689314293d746a184014
dablyoRBT <dablyoteixeira@gmail.com>
2026-01-17 20:30:11 -0300

    refactor: simplificar o codigo, separar validação e cálculo em funções

--- calculadora.py
+++ calculadora.py
@@ -1,37 +1,33 @@
-operacoes = ["+", "-", "/", "*"]
-
-while True:
-    try:
-        n1 = float(input("Digite um valor: "))
-        op = input("Digite a operação: ")
-        if op in operacoes:
-            n2 = float(input("Digite outro valor: "))
-        else:
-            print("Operação inválida!")
-            continue
-    except ValueError:
-        print("Você digitou um valor inválido!")
-    else:
-        break
-
-match op:
+def validar():
+    operacoes = ["+", "-", "/", "*"]
+    while True: 
+        try: 
+            n1 = float(input("Digite um valor: ")) 
+            op = input("Digite a operação: ") 
+            if op in operacoes: 
+                n2 = float(input("Digite outro valor: ")) 
+            else: 
+                print("Operação inválida!") 
+                continue 
+        except ValueError: print("Você digitou um valor inválido!") 
+        else: 
+            resultado = calcular(n1, n2, op)
+            print(f"Resultado: {resultado}")
+            break
+        
+def calcular(value1, value2, operation):
+ match operation:
     case "+":
-        if n1.is_integer() and n2.is_integer() :
-            print(int(n1 + n2))
-        else:
-            print(float(n1 + n2))
+        return value1 + value2
     case "-":
-        if n1.is_integer() and n2.is_integer() :
-            print(int(n1 - n2))
-        else:
-            print(float(n1 - n2))
+        return value1 - value2
     case "*":
-        if n1.is_integer() and n2.is_integer() :
-            print(int(n1 * n2))
-        else:
-            print(float(n1 * n2))
+        return value1 * value2
     case "/":
         try:
-            print(n1 / n2)
+            return value1 / value2
         except ZeroDivisionError:
-            print("Indefinido!")
+            return "Indefinido!"
+            
+            
+validar()
