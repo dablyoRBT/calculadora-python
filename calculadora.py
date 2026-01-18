4fc2699b21c2baeed0e769600d4a3de57c36736c
dablyoRBT <dablyoteixeira@gmail.com>
2026-01-17 23:38:49 -0300

    feat: adiciona suporte a múltiplas operações e melhora arquitetura

--- calculadora.py
+++ calculadora.py
@@ -1,33 +1,55 @@
+#Validação de entrada inicial ------------------------------------------------------------
+operacoes = ["+", "-", "/", "*", "="]
 def validar():
-    operacoes = ["+", "-", "/", "*"]
     while True: 
         try: 
             n1 = float(input("Digite um valor: ")) 
-            op = input("Digite a operação: ") 
-            if op in operacoes: 
+            operacao = input("Digite a operação: ") 
+            if operacao in operacoes: 
                 n2 = float(input("Digite outro valor: ")) 
             else: 
                 print("Operação inválida!") 
                 continue 
         except ValueError: print("Você digitou um valor inválido!") 
         else: 
-            resultado = calcular(n1, n2, op)
-            print(f"Resultado: {resultado}")
+            resultado = calcular(n1, n2, operacao)
+            loop(resultado)
             break
-        
+
+#Calculadora -----------------------------------------------------------------------------     
 def calcular(value1, value2, operation):
- match operation:
-    case "+":
-        return value1 + value2
-    case "-":
-        return value1 - value2
-    case "*":
-        return value1 * value2
-    case "/":
+    match operation:
+        case "+":
+            return value1 + value2
+        case "-":
+            return value1 - value2
+        case "*":
+            return value1 * value2
+        case "/":
+            try:
+                return value1 / value2
+            except ZeroDivisionError:
+                return "Indefinido!"  
+            
+#Laço principal de repetições ------------------------------------------------------------
+def loop(result):
+    atual = result
+    while True:
         try:
-            return value1 / value2
-        except ZeroDivisionError:
-            return "Indefinido!"
-            
-            
+            operacao = input("Digite outra operação ou = para finalizar: ")
+            if operacao in operacoes: 
+                if operacao == "=":
+                    print(f"Resultado: {atual}")
+                    break
+                else:
+                    add = float(input("Digite outro valor: "))
+                    atual = calcular(atual, add, operacao)
+                    continue  
+            else: 
+                print("Operação inválida!") 
+                continue 
+        except ValueError:
+            print("Você digitou um valor inválido!")
+        
+    
 validar()
