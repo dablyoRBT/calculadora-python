f69d5ad53737c38fc4d15b57ff7ba23958eca7be
Dablyo Teixeira <dablyoteixeira@gmail.com>
2026-01-19 13:59:07 -0300

    feat: Implementa precedência de operadores matemáticos e melhorias de fluxo

--- calculadora.py
+++ calculadora.py
@@ -1,19 +1,3 @@
-#Validação de entrada inicial ------------------------------------------------------------
-operacoes = ["+", "-", "/", "*", "="]
-def obter_entrada():
-    while True: 
-        try: 
-            n1 = float(input("Digite um valor: ")) 
-            operacao = input("Digite a operação: ") 
-            if operacao in operacoes: 
-                n2 = float(input("Digite outro valor: ")) 
-            else: 
-                print("Operação inválida!") 
-                continue 
-        except ValueError: print("Você digitou um valor inválido!") 
-        else: 
-            return n1, n2, operacao
-
 #Calculadora -----------------------------------------------------------------------------     
 def calcular(value1, value2, operation):
     match operation:
@@ -27,21 +11,40 @@ def calcular(value1, value2, operation):
             try:
                 return value1 / value2
             except ZeroDivisionError:
-                return "Indefinido!"  
+                return "ZeroDivisionError"
+
+
+#Validação de entrada inicial ------------------------------------------------------------
+def obter_entrada():
+    operacoes = ["+", "-", "/", "*"]
+    while True: 
+        try: 
+            n1 = float(input("Digite um valor: ")) 
+            operacao = input("Digite a operação: ") 
+            if operacao in operacoes: 
+                n2 = float(input("Digite outro valor: ")) 
+            else: 
+                print("Operação inválida!") 
+                continue 
+        except ValueError: print("Você digitou um valor inválido!") 
+        else: 
+            return [n1, operacao, n2]
             
 #Laço principal de repetições ------------------------------------------------------------
-def executar_operacoes(result):
-    atual = result
+def montar_operacao(entrada):
+    operacoes = ["+", "-", "/", "*", "="]
+    expressao = entrada
     while True:
         try:
             operacao = input("Digite outra operação ou = para finalizar: ")
             if operacao in operacoes: 
                 if operacao == "=":
-                    print(f"Resultado: {atual}")
-                    break
+                    return expressao
+                    
                 else:
                     add = float(input("Digite outro valor: "))
-                    atual = calcular(atual, add, operacao)
+                    expressao.append(operacao)
+                    expressao.append(add)
                     continue  
             else: 
                 print("Operação inválida!") 
@@ -49,24 +52,77 @@ def executar_operacoes(result):
         except ValueError:
             print("Você digitou um valor inválido!")
         
+#Define a ordem de precedência dentro da expressão matemática ----------------------------
+def precedencia(array):
+    i = 0
+    while i < len(array):
+        if array[i] == "*" or array[i] == "/":
+            x = i - 1  # Número antes
+            y = i + 1  # Número depois
+            z = calcular(array[x], array[y], array[i]) # Realiza o cálculo
+            if z == "ZeroDivisionError":
+                return "ZeroDivisionError"
+            else:
+                array.pop(y) #
+                array.pop(i) # Remove os valores calculados da lista e insere o resultado no lugar
+                array.pop(x) #
+                
+                array.insert(x, z) # Resultado
+                
+                i = 0 # Set para zero, pois a lista mudou...
+        else:
+            i += 1
+    
+    i = 0
+    while i < len(array):
+        if array[i] == "+" or array[i] == "-":
+            x = i - 1 
+            y = i + 1  
+            z = calcular(array[x], array[y], array[i])
+
+            array.pop(y) 
+            array.pop(i) 
+            array.pop(x) 
+            array.insert(x, z)
+            
+            i = 0 
+        else:
+            i += 1
+    
+    return array[0]
+        
+
 #Controle do programa -------------------------------------------------------------------------
 def main():
-    entrada = obter_entrada()
-    resultado = calcular(*entrada)
-    executar_operacoes(resultado)
-    while True:
-        end = input("Deseja continuar? (s/n): ").upper()
+    while True: 
+        dados = obter_entrada()
+        expressao_final = montar_operacao(dados)
+        resultado = precedencia(expressao_final)
+        if resultado == "ZeroDivisionError":
+            print("Erro de divisão por zero!")
+            print("Reiniciando programa...")
+            continue
+        else:
+            print(f"O resultado é: {resultado}")
+            while True:
+                end = input("Deseja continuar? (s/n): ").upper()
+                if end == "S" or end == "N":
+                    break
+                else:
+                    print("Por favor, digite S para sim ou N para não!")
+                    continue
         if end == "S":
-            entrada = obter_entrada()
-            resultado = calcular(*entrada)
-            executar_operacoes(resultado)
             continue
         elif end == "N":
             print("Fim do programa!")
             break
-        else:
-            print("Por favor, digite S para sim ou N para não!")
-            continue
-        
-
+                    
 main()
+
+
+
+
+
+
+
+
